4c0b17e57d837260bda80d38172a038960ece968
antoinechedin <antoine.chedin@gmail.com>
2019-11-20 11:04:11 +0100

    Add player action collider

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Player : Actor
 {
     public float moveSpeed;
-    public PathfindingManager pathfinding;
+
+    public BoxCollider2D actionCollider;
 
     private void Update()
     {
@@ -13,6 +14,10 @@ public class Player : Actor
         float moveMag = Mathf.Clamp(moveDir.magnitude, 0f, 1f);
         moveDir = moveDir.normalized * moveMag;
         moveVec = moveDir * moveSpeed;
+
+        // Move action object
+        if (actionCollider != null && moveDir.magnitude > 0)
+            actionCollider.transform.localPosition = new Vector2(0, 0.5f) + moveDir.normalized / 2f;
 
         if (moveVec.x > 0) sr.flipX = false;
         if (moveVec.x < 0) sr.flipX = true;
