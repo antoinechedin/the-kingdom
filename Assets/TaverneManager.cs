d8612d923932efb9b62c7c0f47f3f4ebb7af157f
DedeLeBorgne <55095246+DedeLeBorgne@users.noreply.github.com>
2019-12-13 17:50:59 +0100

    Create taverneManager

---
+++ Assets/TaverneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaverneManager : MonoBehaviour
+{
+    public int currentHPTavern;
+    public int maxHPTavern;
+    private BoxCollider2D frontDoorCollider;
+    private void Start()
+    {
+        currentHPTavern = maxHPTavern;
+        frontDoorCollider = GetComponent<BoxCollider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D thingThatCollided)
+    {
+        if (thingThatCollided.tag == "Monster")
+        {
+            currentHPTavern = currentHPTavern - 1; //ou on aurait pu l'écrire currentHPTavern -= 1; currentHPTavern--;  
+            Destroy(thingThatCollided.gameObject);
+            if (currentHPTavern == 0)
+            {
+                Debug.Log("T'as perdu gros nul");
+            }
+        }
+    }
+}
+
+
