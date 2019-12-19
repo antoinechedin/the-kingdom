a24ba398713016f2acd99e2be393eeba031f4417
antoinechedin <antoine.chedin@gmail.com>
2019-12-19 12:26:40 +0100

    Add Spawner

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -5,7 +5,7 @@ public class Monster : Actor
 {
     public int maxLife;
     public PathfindingManager pm;
-    private GameObject player;
+    public Vector2 target;
     private List<Cell> path;
 
     public float moveSpeed;
@@ -23,8 +23,7 @@ public class Monster : Actor
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        path = pm.FindPath(transform.position, player.transform.position);
+        path = pm.FindPath(transform.position, target);
     }
 
     public void TakeDamage(float damage)
@@ -38,38 +37,29 @@ public class Monster : Actor
     private void Update()
     {
         moveVec = Vector2.zero;
-        Vector3 nextTarget = transform.position;
 
-        if (path.Count > 0)
+        if (path != null)
         {
-            nextTarget = path[0].worldPos;
-            moveVec = nextTarget - transform.position;
-            float distance = moveVec.magnitude;
-            moveVec.Normalize();
-            moveVec *= moveSpeed;
+            Vector3 nextTarget = transform.position;
 
-            if (distance < 0.2f)
-                path = pm.FindPath(transform.position, player.transform.position);
+            if (path.Count > 0)
+            {
+                nextTarget = path[0].worldPos;
+                moveVec = nextTarget - transform.position;
+                float distance = moveVec.magnitude;
+                moveVec.Normalize();
+                moveVec *= moveSpeed;
+
+                if (distance < 0.2f)
+                {
+                    path.RemoveAt(0);
+                }
+            }
+            if (moveVec.x > 0) sr.flipX = false;
+            if (moveVec.x < 0) sr.flipX = true;
+            /*if (transform.position.y < player.transform.position.y) sr.sortingOrder = 1;
+            else sr.sortingOrder = -1;*/
         }
-        else
-            path = pm.FindPath(transform.position, player.transform.position);
-        // if ((path[0].worldPos - transform.position).magnitude < 0.3f)
-        // {
-        //     Debug.Log("Catch");
-        //     path.RemoveAt(0);
-        //     if (path.Count == 0)
-        //     {
-        //         Debug.Log("Recalculate path");
-        //         //
-        //         Debug.Log("Path recalculated");
-        //     }
-        // }
-
-        if (moveVec.x > 0) sr.flipX = false;
-        if (moveVec.x < 0) sr.flipX = true;
-        if (transform.position.y < player.transform.position.y) sr.sortingOrder = 1;
-        else sr.sortingOrder = -1;
-
         animator.SetFloat("Speed", moveVec.magnitude / moveSpeed);
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    public PathfindingManager[] pfManagers;
+    public float spawnTime;
+
+    private float timer;
+    private Vector2 start;
+    private Vector2 target;
+
+    private void Awake()
+    {
+        start = transform.position;
+        GetComponent<SpriteRenderer>().sprite = null;
+
+        target = transform.GetChild(0).transform.position;
+        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = null;
+
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > spawnTime)
+        {
+            timer -= spawnTime;
+            SpawnMonster();
+        }
+    }
+
+    private void SpawnMonster()
+    {
+        Monster monster = Instantiate(monsterPrefab, start, Quaternion.identity).GetComponent<Monster>();
+        PathfindingManager randomManager = pfManagers[Random.Range(0, pfManagers.Length)];
+        monster.pm = randomManager;
+        monster.target = target;
+    }
+}
