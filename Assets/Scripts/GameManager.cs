1033a4234d17985c53f32bad44d7ed1438771be5
DedeLeBorgne <55095246+DedeLeBorgne@users.noreply.github.com>
2020-01-06 19:21:22 +0100

    Finish end screen + fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,4 +21,10 @@ public class GameManager : MonoBehaviour
             Debug.Log("Debug mode " + mode);
         }
     }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Map");
+    }
+
 }
