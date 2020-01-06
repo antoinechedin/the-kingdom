e23a7c3c80fa11561ec5687e668739e436b69012
antoinechedin <antoine.chedin@gmail.com>
2020-01-06 23:07:22 +0100

    Add controller management and pause menu

--- Assets/EndScreenManager.cs
+++ Assets/EndScreenManager.cs
@@ -6,13 +6,15 @@ using UnityEngine.UI;
 public class EndScreenManager : MonoBehaviour
 {
     private float timer;
+    public Text menuText;
     public Text surviveTime;
     public GameObject panel;
     public TaverneManager tavern;
     public Player player;
     public GameObject UI;
     private int freezedSurviveTime;
-    private void Awake()
+   
+    private void Start()
     {
         timer = 0;
         freezedSurviveTime = 0;
@@ -21,7 +23,7 @@ public class EndScreenManager : MonoBehaviour
     private void Update()
     {
         timer += Time.deltaTime;
-       
+
         if (tavern.TestifLost() && freezedSurviveTime == 0)
         {
             panel.SetActive(true);
@@ -31,7 +33,25 @@ public class EndScreenManager : MonoBehaviour
             int seconds = (int)this.freezedSurviveTime;
             int minutes = (int)seconds / 60;
             seconds = seconds - (minutes * 60);
+            menuText.text = "GAME OVER\n\nThe beer isn't yours anymore.";
             surviveTime.text = "You survived " + minutes + "min " + seconds + "sec";
+        }
+
+        if (freezedSurviveTime == 0 && Input.GetButtonDown("Start"))
+        {
+            if (player.playing)
+            {
+                panel.SetActive(true);
+                UI.SetActive(false);
+                player.playing = false;
+                menuText.text = "The game is not paused, careful M.Knight";
+                surviveTime.text = "";
+            }
+            else{
+                panel.SetActive(false);
+                UI.SetActive(true);
+                player.playing = true;
+            }
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,23 +19,18 @@ public class Player : Actor
 
     private void Update()
     {
+        Vector2 moveDir = Vector2.zero;
         if (playing)
         {
-            Vector2 moveDir;
             moveDir.x = Input.GetAxisRaw("Horizontal");
             moveDir.y = Input.GetAxisRaw("Vertical");
             float moveMag = Mathf.Clamp(moveDir.magnitude, 0f, 1f);
             moveDir = moveDir.normalized * moveMag;
-            moveVec = moveDir * moveSpeed;
 
             // Move action object
             if (actionCollider != null && moveDir.magnitude > 0)
                 actionCollider.transform.localPosition = new Vector2(0, 0.5f) + moveDir.normalized / 2f;
 
-            if (moveVec.x > 0) sr.flipX = false;
-            if (moveVec.x < 0) sr.flipX = true;
-            animator.SetFloat("Speed", moveDir.magnitude);
-        
             if (currentTurret != null)
             {
                 if (Input.GetButtonDown("Action"))
@@ -52,6 +47,11 @@ public class Player : Actor
                 }
             }
         }
+        
+        moveVec = moveDir * moveSpeed;
+        if (moveVec.x > 0) sr.flipX = false;
+        if (moveVec.x < 0) sr.flipX = true;
+        animator.SetFloat("Speed", moveDir.magnitude);
     }
 
     private void OnTriggerEnter2D(Collider2D thingThatCollide)
